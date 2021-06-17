7117aae713b9889516f4c27cc60b807d9d7926d6
Tesh <54969187+shintom1222@users.noreply.github.com>
2021-06-17 22:29:52 +0900

    Organize by file

---
+++ colors.js
@@ -0,0 +1,40 @@
+var Links = {
+    setColor:function(color) {
+      var alist = document.querySelectorAll('a');
+      var i = 0;
+
+      while (i < alist.length) {
+        alist[i].style.color = color;
+        i = i + 1;
+      }
+    }
+  }
+  var Body = {
+    setColor:function(color) {
+      document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor:function(color) {
+      document.querySelector('body').style.backgroundColor = color;
+    },
+    setBorderColor:function(color) {
+      document.querySelector('body').style.borderColor = color;
+    }
+  }
+  function nightDayHandler(self) {
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      Body.setBorderColor('white');
+      self.value = 'day';
+
+      Links.setColor('lightgreen');
+    } else{
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      Body.setBorderColor('black');
+      self.value = 'night';
+
+      Links.setColor('mediumaquamarine');
+    }
+  }
