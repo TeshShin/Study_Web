af4bbbc277140aea141453b73ce771f037574b49
Tesh <54969187+shintom1222@users.noreply.github.com>
2021-06-17 22:56:08 +0900

    Using jQuery

--- colors.js
+++ colors.js
@@ -1,23 +1,26 @@
 var Links = {
     setColor:function(color) {
-      var alist = document.querySelectorAll('a');
-      var i = 0;
-
-      while (i < alist.length) {
-        alist[i].style.color = color;
-        i = i + 1;
-      }
+    //   var alist = document.querySelectorAll('a');
+    //   var i = 0;
+    //   while (i < alist.length) {
+    //     alist[i].style.color = color;
+    //     i = i + 1;
+    //   }
+    $('a').css('color',color);
     }
   }
   var Body = {
     setColor:function(color) {
-      document.querySelector('body').style.color = color;
+      //document.querySelector('body').style.color = color;
+      $('body').css('color',color);
     },
     setBackgroundColor:function(color) {
-      document.querySelector('body').style.backgroundColor = color;
+      //document.querySelector('body').style.backgroundColor = color;
+      $('body').css('backgroundColor', color);
     },
     setBorderColor:function(color) {
-      document.querySelector('body').style.borderColor = color;
+      //document.querySelector('body').style.borderColor = color;
+      $('body').css('borderColor',color);
     }
   }
   function nightDayHandler(self) {
